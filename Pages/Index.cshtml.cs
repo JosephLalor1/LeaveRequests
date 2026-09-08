613f3967e793ea0b9df5cb953257a0be8f5a1812
Joseph Lalor <josephpatricklalor@gmail.com>
2026-09-08 11:08:51 +0100

    Initial project setup with Identity

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LeaveRequests.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
